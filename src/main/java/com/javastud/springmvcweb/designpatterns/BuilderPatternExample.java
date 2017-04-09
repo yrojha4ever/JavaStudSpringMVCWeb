435b5fc6ac5172b756f91e68a502aecbf297a558
Yadab Raj Ojha <YadabRaj.Ojha@pointclickcare.com>
2017-04-09 22:52:39 +0545

    lombok builder annotaion example added.

--- src/main/java/com/javastud/springmvcweb/designpatterns/BuilderPatternExample.java
+++ src/main/java/com/javastud/springmvcweb/designpatterns/BuilderPatternExample.java
@@ -1,13 +1,20 @@
 package com.javastud.springmvcweb.designpatterns;
 
+import java.time.LocalDate;
+
 public class BuilderPatternExample {
 	public static void main(String[] args) {
-		User user = User.UserBuilder()
+		User user = User.builder()
 						.name("RAM")
 						.age(20)
 						.email("ram@gmail.com")
 						.build();
 		System.out.println(user);
 		
+		
+		Employee emp = Employee.builder().id(5).name("Ram").address("KTM").birthDate(LocalDate.of(1990, 11, 17)).build();
+		System.out.println(emp);
+		
 	}
+	
 }
